251cba2e89491de39078ac374e4943a2481e3ccd
ravi8881 <ravi.dhyani8881@gmail.com>
2017-04-27 08:15:40 +0530

    Added Initial files for web crawler

---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# filename: run.py
+import re
+from crawler import Crawler, CrawlerCache
+
+if __name__ == "__main__": 
+    # Using SQLite as a cache to avoid pulling twice
+    crawler = Crawler(CrawlerCache('crawler.db'))
+    root_re = re.compile('^/$').match
+    crawler.crawl('http://wiprodigital.com', no_cache=root_re)
